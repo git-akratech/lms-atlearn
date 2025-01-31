8a43ae0b1bfe8086a004aee32384af1dc1848827
palanivelraja14 <palanivelraja.akra@gmail.com>
2025-01-31 18:10:24 +0530

    Get_bigblubutton_online_class_courses

---
+++ local/bigbluebutton_courses/classes/bigbluebutton_courses.php
@@ -0,0 +1,37 @@
+<?php
+namespace local_bigbluebutton_courses;
+
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->libdir . '/externallib.php');
+
+class external extends \external_api{
+    
+    public function get_courses_with_bigbluebutton() {
+        global $DB;
+
+        // Get all courses with BigBlueButton activities
+        $sql = "SELECT c.id, c.fullname
+                FROM {course} c
+                JOIN {bigbluebuttonbn} b ON b.course = c.id
+                WHERE b.id IS NOT NULL";
+
+        $courses = $DB->get_records_sql($sql);
+        
+        return array_values($courses);
+    }
+    public static function get_courses_with_bigbluebutton_parameters() {
+        return new \external_function_parameters(
+            []
+        );
+    }
+    public static function get_courses_with_bigbluebutton_returns() {
+    return new \external_multiple_structure(
+        new \external_single_structure(
+            [
+                'id' => new \external_value(PARAM_INT, 'Course ID'),
+                'fullname' => new \external_value(PARAM_TEXT, 'Course Full Name'),
+            ]
+        )
+    );
+}
+}
---
+++ local/bigbluebutton_courses/version.php
@@ -0,0 +1,6 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_bigbluebutton_courses'; // Full name of the plugin (used for diagnostics).
+$plugin->version = 2023100100; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires = 2022041900; // Requires this Moodle version.
