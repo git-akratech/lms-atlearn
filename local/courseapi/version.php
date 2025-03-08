b6d2816b1c9ced3056d7220737253f7f4267a1c2
palanivelraja14 <palanivelraja.akra@gmail.com>
2025-03-08 11:49:16 +0530

    revert_custom_courser_api

--- local/courseapi/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'local_courseapi';
-$plugin->version = 2025030700;
-$plugin->requires = 2021051700;
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
