adcc386480a1bbb9a4304f44ff7898466fb45263
palanivelraja14 <palanivelraja.akra@gmail.com>
2025-04-02 19:12:00 +0530

    zoom-api_user-list-roles-show

---
+++ local/coursezoom/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_coursezoom';
+$plugin->version   = 2025040200;
+$plugin->requires  = 2020110900; // Moodle 3.10+
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
